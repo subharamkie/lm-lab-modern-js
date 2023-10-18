3199abbee9ebf0dd2dbd18e19b954477d926e308
Subha <subharamkie@gmail.com>
2023-10-18 21:21:11 +0100

    Refactor:Using destructuring method to access array elements and objects and use rest parameters for function

--- modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
+++ modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
@@ -1,9 +1,6 @@
 // Instructions can be found in destructuring_arrays.md
 
 export function sequence(order) {
-  const first = order[0];
-  const second = order[1];
-  const third = order[2];
-
-  return first + ", " + second + ", " + third;
+  const [first, second, third] = order;
+  return `${first}, ${second}, ${third}`;
 }
--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,13 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
-  // Add a loop here
-  return a + b;
+export function add(...numbers) {
+  let sum = 0;
+  for (let num of numbers) {
+    sum += num;
+  }
+  console.log(sum);
+  return sum;
 }
 
 add(1, 2, 3, 4, 5);
