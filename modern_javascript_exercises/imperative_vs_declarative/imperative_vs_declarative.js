c07712e1ce2a0ea7545883b70cc0dc18cd00aa92
Subha <subharamkie@gmail.com>
2023-10-18 21:45:49 +0100

    Refactor:Used spread&ternary operators,changed for loop to use filter method and template literals

--- modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
+++ modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
@@ -1,13 +1,6 @@
 // Instructions can be found in imperative_vs_declarative.md
 
 export function longPasswords(passwords) {
-  let longPasswords = [];
-
-  for (let i = 0; i < passwords.length; i++) {
-    const password = passwords[i];
-    if (password.length >= 9) {
-      longPasswords.push(password);
-    }
-  }
+  const longPasswords = passwords.filter((password) => password.length >= 9);
   return longPasswords;
 }
